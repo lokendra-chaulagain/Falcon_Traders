78876f882a76e87c4a8b61d68693c95913066bdf
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-12-19 19:49:50 +0545

    thaal layo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,46 +2,88 @@ import React from "react";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { RiFacebookFill } from "react-icons/ri";
 import { AiFillInstagram } from "react-icons/ai";
-import { BsArrowRight } from "react-icons/bs";
-
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export default function Footer() {
+  const mailSendSuccess = () => toast.success("We will notify our updates");
+  const sendMailError = () => toast.error("Something Went Wrong");
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const handleAllField = watch();
+
+  const createSubscriber = async () => {
+    try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/subscriber`, handleAllField);
+      console.log("Form has been submitted");
+      mailSendSuccess();
+      reset();
+    } catch (error) {
+      console.log(error);
+      sendMailError();
+    }
+  };
+
   return (
     <div className="footer_wrapper">
-      <div className="row">
-        <div className="col-8 d-flex  align-items-center justify-content-center ">
-          <h5 className="fw-semibold h5 dark_blue">SUBSCRIBE TO OUR NEWSLETTER</h5>
-          <input
-          autoComplete="off"
-            type="email"
-            className="form-control w-50 rounded-0 subscribe_input ms-5"
-            placeholder="Your E-mail Here"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <BsArrowRight size={26} color="grey"  />
+      <div className="row  d-flex align-items-center px-2 px-md-5">
+        <div className="col-12 col-lg-4">
+          <h5 className="fw-semibold text-center text-lg-start mb-0 h5 dark_blue">SUBSCRIBE TO OUR NEWSLETTER</h5>
         </div>
-        <div className="col-4 d-flex align-items-center justify-content-center gap-3 dark_blue">
+
+        <form
+          onSubmit={handleSubmit(createSubscriber)}
+          className="col-12 col-lg-4 d-flex align-items-center justify-content-center">
+          <div className="w-100">
+            <div className="d-flex align-items-center mt-4 w-100">
+              <input
+                type="email"
+                className="form-control  rounded-0 subscribe_input ms-0 ms-md-5 my-lg-0"
+                placeholder="Your E-mail Here"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                {...register("email", { required: "Email is required" })}
+              />
+
+              <button
+                type="submit"
+                className="btn btn-primary btn-sm subscribe_button rounded-1">
+                Subscribe
+              </button>
+            </div>
+            {errors.email && <p className="text-danger text-center mt-0 mt-lg-2  fw-semibold ">{`${errors.email.message}`}</p>}
+          </div>
+        </form>
+
+        <div className="col-12 col-lg-4 mt-2  d-flex align-items-center justify-content-center gap-3 dark_blue">
           JOIN US ON
           <AiOutlineTwitter
             size={34}
-            className=" footer_icon_div1 rounded-circle  p-2 ms-5"
+            className=" footer_icon_div1 rounded-circle cp p-2 ms-5"
           />
           <RiFacebookFill
             size={34}
-            className=" footer_icon_div2 rounded-circle  p-2"
+            className=" footer_icon_div2 rounded-circle cp p-2"
           />
           <AiFillInstagram
             size={34}
-            className=" footer_icon_div3 rounded-circle  p-2"
+            className=" footer_icon_div3 rounded-circle cp p-2"
           />
         </div>
       </div>
+
       <hr />
-      <div className="row footer_last_row ">
+      <div className="row footer_last_row px-2 px-md-5 ">
         <div className="col small d-flex align-items-center justify-content-center dark_blue">TERMS & CODITION POLICY</div>
-        <div className="col h4 d-flex align-items-center justify-content-center dark_blue">Logo Here</div>
-        <div className="col d-flex align-items-center justify-content-center dark_blue">© 2019 Bamboo All Rights Reserved</div>
+        {/* <div className="col d-none d-md-block h4 d-flex align-items-center justify-content-center dark_blue">Logo Here</div> */}
+        <div className="col small d-flex align-items-center justify-content-center dark_blue">© 2019 Bamboo All Rights Reserved</div>
       </div>
     </div>
   );
--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -7,29 +7,23 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
 import { BsArrowRight } from "react-icons/bs";
-import logo from "../asset/logo.png";
 
 export default function About() {
   return (
-    <div className="px-5 my-5 pt-5">
-      <div className="about_hero d-flex  flex-column align-items-center justify-content-center ">
-        <div className="about_logo_div ">
-          <Image
-            src={logo}
-            layout="fill"
-            objectFit="cover"
-            alt=""
-          />
+    <div className="about_page_wrapper">
+      <div className="div d-flex align-items-center justify-content-center">
+        <div className="about_hero d-flex flex-column align-items-center justify-content-center ">
+          <h1 className="h1 dark_blue">Who We Are</h1>
+          <p className="mt-3 fz18 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat libero, praesent quam enim. Sit tempus fermentum nunc mi rhoncus ultricies sit. Sit mauris eget eget ornare nec vestibulum tellus porta.</p>
         </div>
-        <p className="text-center mt-5 pt-4 fz18">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat libero, praesent quam enim. Sit tempus fermentum nunc mi rhoncus ultricies sit. Sit mauris eget eget ornare nec vestibulum tellus porta.</p>
       </div>
 
       <div className="grey_bg ">
+        {/* First Image text section--------------- */}
 
-        {/* ------------------------------------------- */}
-        <div className="row my-">
-          <div className="col-12  col-xl-4 p-0 d-flex flex-column  justify-content-center">
-            <h1 className="h1 fw-bold dark_blue">Best Place to get your Interior Needs</h1>
+        <div className="row my-5 ">
+          <div className="order-2 mt-4 mt-xl-0 col-12 col-xl-4 p-0  pe-0 pe-xl-3   d-flex flex-column  justify-content-center">
+            <h1 className="h1 fw-bold dark_blue">Our Values</h1>
             <h6 className="text-muted mb-3">Best Place to get your Interior Needs</h6>
 
             <p className="fz18 para_color">
@@ -37,7 +31,8 @@ export default function About() {
               convallis libero nec.
             </p>
           </div>
-          <div className="col p-0 ps-3">
+
+          <div className="order-1 order-xl-2 col-lg-12 col-xl-8 p-0   ">
             <div className="about_sec_1_right_img_div">
               <Image
                 src={banner}
@@ -49,9 +44,10 @@ export default function About() {
           </div>
         </div>
 
-        <div className="div d-flex align-items-center justify-content-center my-5 py-5">
-          <div className="row  col-8 ">
-            <div className="about_sec_1_right_img_div">
+        {/* Second Image text section--------------- */}
+        <div className="div  d-flex align-items-center justify-content-center my-5 py-5">
+          <div className="row  col-12 col-xl-8  ">
+            <div className="about_sec_1_right_img_div ">
               <Image
                 src={banner}
                 layout="fill"
@@ -59,18 +55,19 @@ export default function About() {
                 alt=""
               />
             </div>
-            <h1 className="dark_blue fw-bold my-2">Our Story</h1>
-            <h6 className="text-muted mb-3">Best Place to get your Interior Needs</h6>
+            <h1 className="dark_blue fw-bold my-2 p-0 mt-3 mt-xl-0">Our Story</h1>
+            <h6 className="text-muted mb-3 p-0">Best Place to get your Interior Needs</h6>
 
-            <p className="fz18">
+            <p className="fz18 p-0">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, et fermentum eget urna. Turpis sodales massa mi est. Lectus tincidunt maecenas auctor purus, egestas ornare dui. Cursus enim turpis id arcu sagittis dignissim quam justo. Eget neque mus semper ac dictumst. Sit volutpat pellentesque elementum integer dictum convallis ullamcorper nibh. Mi ante sed risus eu porta nibh morbi lorem a. Diam tincidunt pellentesque est sed justo vulputate rhoncus ut. Faucibus maecenas
               convallis libero nec. Lobortis suscipit neque pellentesque et lectus dolor.
             </p>
           </div>
         </div>
 
+        {/* Last image text section------------- */}
         <div className="row my-5 ">
-          <div className="col p-0 pe-4">
+          <div className=" col-lg-12 col-xl-8 p-0 pe-0 pe-xl-3">
             <div className="about_sec_1_right_img_div">
               <Image
                 src={banner}
@@ -80,7 +77,7 @@ export default function About() {
               />
             </div>
           </div>
-          <div className="col-4 p-0 d-flex flex-column  justify-content-center">
+          <div className="col-12 col-xl-4 p-0 mt-4 mt-xl-0 d-flex flex-column  justify-content-center">
             <h1 className="h1 fw-bold dark_blue">Our Values</h1>
             <h6 className="text-muted mb-3">Best Place to get your Interior Needs</h6>
 
@@ -92,9 +89,9 @@ export default function About() {
         </div>
 
         <div className="row about_swiper_section my-5">
-          <h1 className="dark_blue fw-bold my-2">Our Products</h1>
-          <h6 className="text-muted mb-3">Best Place to get your Interior Needs</h6>
-          <p className="fz18 pe-5 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, et fermentum eget urna. Turpis sodales massa mi est. Lectus tincidunt maecenas auctor purus, egestas ornare dui. Cursus enim turpis id arcu sagittis dignissim quam justo. Eget neque mus semper ac dictumst.</p>
+          <h1 className="dark_blue fw-bold my-2 p-0">Our Products</h1>
+          <h6 className="text-muted mb-3 p-0">Best Place to get your Interior Needs</h6>
+          <p className="fz18 pe-5 mb-4 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, et fermentum eget urna. Turpis sodales massa mi est. Lectus tincidunt maecenas auctor purus, egestas ornare dui. Cursus enim turpis id arcu sagittis dignissim quam justo. Eget neque mus semper ac dictumst.</p>
 
           <Swiper
             slidesPerView={3}
